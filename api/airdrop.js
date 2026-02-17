74333e8f0a9b7649d0f8cd3eb314286c731903d2
thumb2086 <cpxru83@gmail.com>
2026-02-17 23:27:49 +0800

    Remove blank line in airdrop.js
    Remove unnecessary blank line at the beginning of the file.

--- api/airdrop.js
+++ api/airdrop.js
@@ -1,6 +1,5 @@
 import { ethers } from "ethers";
 
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
