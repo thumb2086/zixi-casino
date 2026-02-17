1f9bdd82185b14a25b415b5f1d795772f87f27ad
thumb2086 <cpxru83@gmail.com>
2026-02-17 23:01:33 +0800

    Add blank line for improved readability in airdrop.js

--- api/airdrop.js
+++ api/airdrop.js
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
